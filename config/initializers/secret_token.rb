dc3aaded68ea38c3de24a748befa25943c6437c6
Paul Elliott <paul@hashrocket.com>
2011-04-28 14:08:02 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProofingOven::Application.config.secret_token = '6d8a012893fc610ebe33766aa496119aa535e4f3ab8bd369de28253e15003aa7c3ea85343a8a37b6b4d1e10f8bd3a454b2d0be7d3665ac082ea9a9cbec941077'
