717febb4c50a2e7dfb2956e888ccffd0a81f8c34
Shane Riley <shane@shaneriley.info>
2011-04-29 16:25:19 -0400

    Create supporting files JS

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,15 @@
 $(function() {
+  $("input.multiple[type=file]").change(function(e) {
+    var $e = $(this),
+        new_id =  +$e.attr("name").match(/\[\d\]/).toString().replace(/\[|\]/g, "");
+    $("<li />").appendTo($e.closest("ul"));
+    $("<input />", {
+      type: "file",
+      name: $e.attr("name").replace(/\[\d\]/, "[" + (++new_id) + "]"),
+      change: e.handler
+    }).appendTo($e.closest("ul").find("li").eq(-1));
+  });
+
   $("input[placeholder]").each(function() {
     var $e = $(this),
         placeholder = $e.attr("placeholder");
