9866918230b33f2daecb8511f7a6dcae038f7ca0
Paul Elliott <paul@hashrocket.com>
2011-04-29 14:12:52 -0400

    Add dummy_image helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def dummy_image(width, height, bg_color="cccccc", fg_color="333333")
+    "http://dummyimage.com/#{width}X#{height}/#{bg_color}/#{fg_color}"
+  end
+
 end
