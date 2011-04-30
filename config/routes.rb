cee8e3f98915958c0ae6bb834026ffcd8926fef0
Alex Aguilar and Lar Van Der Jagt <dev+alexagui+supaspoida@hashrocket.com>
2011-04-30 13:36:31 -0400

    Integrate wireframe for new candidate form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ ProofingOven::Application.routes.draw do
   resources :candidates, only: [:index, :new, :create]
 
   match 'ui(/:action)', controller: 'ui'
-  root to: 'ui#index'
+  root to: 'candidates#index'
 end
