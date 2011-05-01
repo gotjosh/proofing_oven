e4c2de04fc282add413ce21046ccbabc10e7f06b
John Perkins and Paul Elliott <dev+jeperkins4+paulelliott@hashrocket.com>
2011-05-01 16:03:07 -0400

    Move candidate exposures to authenticated controller

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -1,3 +1,6 @@
 class AuthenticatedController < ApplicationController
   before_filter :authenticate_user!
+
+  expose(:all_candidates) { current_user.candidates }
+  expose(:candidate)
 end
--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,7 +1,5 @@
 class CandidatesController < AuthenticatedController
-  expose(:all_candidates) { current_user.candidates }
   expose(:candidates) { current_user.candidates.asc(:last_name, :first_name) }
-  expose(:candidate)
 
   def create
     respond_with(candidate.tap(&:save))
